c5098d7bc213c2938ca4cd596ee79ba22b7b5ac3
DanqingYANG <lenayoungydq@gmail.com>
2015-12-01 20:39:52 +0100

    Ribon Button

---
+++ 2.2 HelloWorld/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace _2._2_HelloWorld
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Automatic)]
+    public class HelloWorld : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello world!");
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ 2.3 CsAddpanel/CsAddPanel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.Windows.Media.Imaging;
+
+namespace _2._3_AddPanel
+{
+    public class CsAddPanel:Autodesk.Revit.UI.IExternalApplication
+    {
+
+        public Autodesk.Revit.UI.Result OnShutdown(Autodesk.Revit.UI.UIControlledApplication application)
+        {
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+
+        public Autodesk.Revit.UI.Result OnStartup(Autodesk.Revit.UI.UIControlledApplication application)
+        {
+            RibbonPanel ribbonPanel = application.CreateRibbonPanel("NewRibbonPanel");
+            //PushButtonData pbd = new PushButtonData("name", "text", @"C:\Users\einst\Desktop\Danqing\RevitLab\2.2 HelloWorld\bin\Debug\2.2 HelloWorld.dll", "HelloWorld");
+            PushButton pushBotton = ribbonPanel.AddItem(new PushButtonData("HelloWorld", "HelloWorld", @"C:\Users\einst\Desktop\Danqing\RevitLab\2.2 HelloWorld\bin\Debug\2.2 HelloWorld.dll", "_2._2_HelloWorld.HelloWorld")) as PushButton;
+            
+            Uri urlImage = new Uri(@"C:\Users\einst\Desktop\Danqing\UrlImage\01.jpg");
+            BitmapImage largeImage = new BitmapImage(urlImage);
+            pushBotton.LargeImage = largeImage;
+
+            return Result.Succeeded;
+        }
+    }
+}
