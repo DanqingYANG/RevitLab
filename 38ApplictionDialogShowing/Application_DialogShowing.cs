f10b3df4e25b3248fc960b846c26bdc3967cd502
DanqingYANG <lenayoungydq@gmail.com>
2015-12-10 19:42:48 +0100

    virtual method

---
+++ 38ApplictionDialogShowing/Application_DialogShowing.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+
+namespace _38ApplictionDialogShowing
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Application_DialogShowing: IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            application.DialogBoxShowing += 
+                new EventHandler<Autodesk.Revit.UI.Events.DialogBoxShowingEventArgs>(AppDialogShowing);
+            return Result.Succeeded;
+        }
+
+        //void application_DialogBoxShowing(object sender, Autodesk.Revit.UI.Events.DialogBoxShowingEventArgs e)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            application.DialogBoxShowing -=
+                new EventHandler<Autodesk.Revit.UI.Events.DialogBoxShowingEventArgs>(AppDialogShowing);
+            return Result.Succeeded;
+        }
+
+        public void AppDialogShowing(object sender, Autodesk.Revit.UI.Events.DialogBoxShowingEventArgs arg)
+        {
+            //get the help id of the showing dialog
+            int dialogId = arg.HelpId;
+
+            //Format the prompt information string
+            string promptInfo = "lalala";
+            promptInfo += "HelpId : " + dialogId.ToString();
+
+            //Show the prompt information and allow the user close the dialog directly
+            TaskDialog td = new TaskDialog("taskDialog1");
+            td.MainContent = promptInfo;
+            TaskDialogCommonButtons buttons = TaskDialogCommonButtons.Ok| TaskDialogCommonButtons.Cancel;
+            td.CommonButtons = buttons;
+            //??liuzbkuv mjhglku
+            TaskDialogResult tdr = td.Show();
+            if (TaskDialogResult.Cancel == tdr)
+            {
+                //Do not show the Revit dialog
+                arg.OverrideResult(1);
+            }
+            else
+            {
+                //Continue to show the Revit dialog
+                arg.OverrideResult(0);
+            }
+        }
+    }
+}
---
+++ ExerciseCSharpTutorialGLS1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseCSharpTutorialGLS1
+{
+    class Program
+    {
+        public class Test
+        {
+            static void Main()
+            {
+                aPoint aPt = new aPoint(0,0,0);
+                aRectang aRct = new aRectang(1,2);
+                aCircle aCir = new aCircle(2.1);
+                Console.WriteLine("Test start: ");
+
+                aPt.display();
+                aPt.getArea(aPt);
+                aRct.getArea(aRct);
+                aCir.getArea(aCir);
+            }
+        }
+
+        public class aPoint
+        {
+            private double P_lx, P_ly, P_lz;
+            public double Area;
+            public aPoint(double x , double y, double z)
+            {
+                P_lx = x;
+                P_ly = y;
+                P_lz = z;
+                Area = 0;
+            }
+            public aPoint()
+            {
+                P_lx = 0;
+                P_ly = 0;
+                P_lz = 0;
+                Area = 0;
+            }
+
+            public virtual void display()
+            {
+                Console.WriteLine("Location: {0},{1},{2}",P_lx, P_ly,P_lz);
+                Console.Write("Area is ");
+            }
+            public void getArea(aPoint aB)
+            {
+                aB.display();
+                Console.WriteLine(Area);
+            }
+
+        }
+
+        class aRectang : aPoint
+        {
+            private double P_l, P_w;
+            public aRectang(double l, double w)
+            {
+                P_l = l;
+                P_w = w;
+                Area = l * w;
+            }
+            public override void display()
+            {
+                base.display();
+                Console.WriteLine(Area);
+            }
+        }
+
+        public class aCircle: aPoint
+        {
+            private double P_r;
+            public aCircle(double r)
+            {
+                P_r = r;
+                Area = 3.14 * r * r;
+            }
+            public override void display()
+            {
+                base.display();
+                Console.WriteLine(Area);
+            }
+        }
+    }
+}
